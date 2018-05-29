8db918a1922f895e331db3115465697d99172ac4
Innocent <31923567+angwandi@users.noreply.github.com>
2018-05-29 13:18:47 +0100

    Clean PetDbHelper, PetProvider and CatalogActivity

--- app/src/main/java/com/example/android/pets/data/PetDbHelper.java
+++ app/src/main/java/com/example/android/pets/data/PetDbHelper.java
@@ -4,12 +4,13 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import static com.example.android.pets.data.PetContract.*;
+import com.example.android.pets.data.PetContract.PetEntry;
 
 /**
  * Database helper for Pets app. Manages database creation and version management.
  */
 public class PetDbHelper extends SQLiteOpenHelper {
+    public static final String LOG_TAG = PetDbHelper.class.getSimpleName();
     /**
      * Name of the database file
      */
@@ -34,13 +35,13 @@ public class PetDbHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         // Create a String that contains the SQL statement to create the pets table
-        String SQL_CREATE_PETS_TABLE = "CREATE TABLE " + PetEntry.TABLE_NAME + "("
-                + PetEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
-                + PetEntry.COLUMN_PET_NAME + " TEXT NOT NULL,"
-                + PetEntry.COLUMN_PET_BREED + " TEXT,"
-                + PetEntry.COLUMN_PET_GENDER + " INTEGER NOT NULL,"
+        String SQL_CREATE_PETS_TABLE = "CREATE TABLE " + PetEntry.TABLE_NAME + " ("
+                + PetEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + PetEntry.COLUMN_PET_NAME + " TEXT NOT NULL, "
+                + PetEntry.COLUMN_PET_BREED + " TEXT, "
+                + PetEntry.COLUMN_PET_GENDER + " INTEGER NOT NULL, "
                 + PetEntry.COLUMN_PET_WEIGHT + " INTEGER NOT NULL DEFAULT 0);";
-//        Execute the SQL statement
+        // Execute the SQL statement
         db.execSQL(SQL_CREATE_PETS_TABLE);
     }
 
@@ -48,7 +49,7 @@ public class PetDbHelper extends SQLiteOpenHelper {
      * This is called when the database needs to be upgraded.
      */
     @Override
-    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         // The database is still at version 1, so there's nothing to do be done here.
     }
 }
